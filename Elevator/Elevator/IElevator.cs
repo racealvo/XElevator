be87b189485e254a5e89936ca223910baeb1d1f0
Ray Salvo <rsalvo@roamdata.com>
2018-04-03 17:57:52 -0400

    next elevator

---
+++ Elevator/Elevator/IElevator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    interface IElevator
+    {
+        int GetCurrentFloor();
+        Direction GetStatus();
+        void AddFloor();
+        ICollection GetFloors();
+    }
+}
